2ef9f04be0a5e1ff8810728583d934f68b3b9721
youngsea <1246787321@qq.com>
2024-11-17 00:31:44 +0800

    1. Establish STM32MP157_A7_LVGL 2. Modify the TARGET in STM32MP157_A7_Multi_Core's Makefile
    zh:
    1. 建立STM32MP157_A7_LVGL工程
    2. 修改STM32MP157_A7_Multi_Core的Makefile里的TARGET

---
+++ example/STM32MP157_A7_LVGL/Application/Inc/stm32mp1xx_atk_conf.h
@@ -0,0 +1,54 @@
+/** 
+  ******************************************************************************
+  * @file    stm32mp1xx_atk_conf.h
+  * @author  MCD Application Team
+  * @brief   configuration file.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2021 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __STM32MP1XX_ATK_CONFIG_H
+#define __STM32MP1XX_ATK_CONFIG_H
+
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+#include "stm32mp1xx_hal.h"
+
+
+/* Activation of Critical Section */
+#define USE_BSP_CRITICAL_SECTION            0U
+
+/* Usage of COM feature */
+#define USE_BSP_COM_FEATURE                 1U
+/* Activation of COM port for log output */
+#define USE_COM_LOG                         1U
+
+#if (USE_BSP_CRITICAL_SECTION == 1)
+#include "lock_resource.h"
+#define BSP_ENTER_CRITICAL_SECTION          PERIPH_LOCK
+#define BSP_EXIT_CRITICAL_SECTION           PERIPH_UNLOCK
+#else
+#define BSP_ENTER_CRITICAL_SECTION(periph)  ((void)0)
+#define BSP_EXIT_CRITICAL_SECTION(periph)   ((void)0)
+#endif
+
+/* IRQ priorities */
+#define BSP_PMU1_IT_PRIORITY                0x03UL
+#define BSP_BUTTON_WAKEUP_IT_PRIORITY       0x0FUL
+#define BSP_BUTTON_KEY0_IT_PRIORITY         0x0FUL
+#define BSP_BUTTON_KEY1_IT_PRIORITY         0x0FUL
+
+#endif /* __STM32MP1XX_ATK_CONFIG_H */
+
